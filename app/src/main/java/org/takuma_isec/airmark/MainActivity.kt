8bb8ed232b6df79c7c33e01fe620c0a755ce853d
Kaniyama_t <webmaster@kaniyama.net>
2019-07-25 15:07:20 +0900

    [fix] Upgraded Kotlin Version 1.3.31 and converted androidx from support

--- app/src/main/java/org/takuma_isec/airmark/MainActivity.kt
+++ app/src/main/java/org/takuma_isec/airmark/MainActivity.kt
@@ -1,7 +1,7 @@
 package org.takuma_isec.airmark
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
