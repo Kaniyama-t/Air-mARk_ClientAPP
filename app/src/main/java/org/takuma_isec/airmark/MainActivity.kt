24921e423afb782bad880864ebe7c9809a50ff03
Kaniyama_t <webmaster@kaniyama.net>
2019-08-02 14:24:28 +0900

    [Add] MainActivity: ARFragment(com.google.ar.sceneorm.ux) Added

--- app/src/main/java/org/takuma_isec/airmark/MainActivity.kt
+++ app/src/main/java/org/takuma_isec/airmark/MainActivity.kt
@@ -2,11 +2,17 @@ package org.takuma_isec.airmark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.ar.sceneform.ux.ArFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.MainActivity_ARFrame, ArFragment())
+            .commit()
+        Toast.makeText(this,"Welcome to Air mARk",Toast.LENGTH_SHORT).show()
     }
 }
